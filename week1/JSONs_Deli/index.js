34e73e7b6fbfc056b0d920b66ca2e4ab77eb766a
shannonstevens73 <shannon.stevens@bryanuniversity.edu>
2020-06-05 17:15:40 -0500

    first commit

---
+++ week1/JSONs_Deli/index.js
@@ -0,0 +1,65 @@
+
+let menu = `{
+    "appetizer": [
+        {
+        "name": "appetizer 1",
+        "ingredients": ["ingredient 1", "ingredient 2", "ingredient 3"],
+        "description": "So yummy!"
+        },
+        {
+        "name": "appetizer 2",
+        "ingredients": ["ingredient 1", "ingredient 2", "ingredient 3"],
+        "description": "I love this one!"
+        }
+    ],
+    "cold sandwich": [
+        {
+        "name": "cold sandwich 1",
+        "ingredients": ["ingredient 1", "ingredient 2", "ingredient 3"],
+        "description": "So yummy!"
+        },
+        {
+        "name": "cold sandwich 2",
+        "ingredients": ["ingredient 1", "ingredient 2", "ingredient 3"],
+        "description": "I love this one!"
+        }
+    ],
+    "hot sandwiches": [
+        {
+        "name": "hot sandwich 1",
+        "ingredients": ["ingredient 1", "ingredient 2", "ingredient 3"],
+        "description": "So yummy!"
+        },
+        {
+        "name": "hot sandwich 2",
+        "ingredients": ["ingredient 1", "ingredient 2", "ingredient 3"],
+        "description": "I love this one!"
+        }
+    ],
+    "dessert": [
+        {
+        "name": "dessert 1",
+        "ingredients": ["ingredient 1", "ingredient 2", "ingredient 3"],
+        "description": "So yummy!"
+        },
+        {
+        "name": "dessert 2",
+        "ingredients": ["ingredient 1", "ingredient 2", "ingredient 3"],
+        "description": "I love this one!"
+        }
+    ],
+    "drinks": [
+        {
+        "name": "drink 1",
+        "ingredients": ["ingredient 1", "ingredient 2", "ingredient 3"],
+        "description": "So yummy!"
+        },
+        {
+        "name": "drink 2",
+        "ingredients": ["ingredient 1", "ingredient 2", "ingredient 3"],
+        "description": "I love this one!"
+        }
+    ]
+}`
+
+console.log(JSON.parse(menu));
