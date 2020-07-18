f518c2a958c86643f23e528a037964334eb3dd0b
shannonstevens73 <shannon.stevens@bryanuniversity.edu>
2020-07-18 14:37:00 -0500

    third commit

--- week7/capstone_project/app.js
+++ week7/capstone_project/app.js
@@ -7,26 +7,67 @@ function get(){
     axios.get(baseURL)
         .then( response => {
             for (let i = 0; i < response.data.length; i++){
-                var title = document.createElement("h3")                
-                var description = document.createElement("p")
-                var deleteButton = document.createElement("button")
+                const title = document.createElement("h3")                
+                const description = document.createElement("p")
+                const completeBox = document.createElement("input")
+                const completeSpan = document.createElement("span")
+                const incompleteBox = document.createElement("input")
+                const incompleteSpan = document.createElement("span")
+                const deleteButton = document.createElement("button")
                 deleteButton.className = "deleteButton"
 
                 title.textContent = response.data[i].title
                 title.style.color = "Navy"    
                 title.style.paddingTop = "20px"         
-                title.style.borderTop = "2px solid lightgray"   
+                title.style.borderTop = "2px solid lightgray" 
+                completeBox.className = "boxComplete"
+                completeBox.type = "checkbox"
+                completeSpan.textContent = "Complete"
+                incompleteBox.className = "boxIncomplete"
+                incompleteBox.type = "checkbox"
+                incompleteSpan.textContent = "Incomplete"
                 description.textContent = response.data[i].description                  
                 deleteButton.textContent = "Delete"    
                 deleteButton.style.backgroundColor = "lightgray"
 
                 document.getElementById("axiosTodo").appendChild(title)                
                 document.getElementById("axiosTodo").appendChild(description)
+                document.getElementById("axiosTodo").append(completeSpan)
+                document.getElementById("axiosTodo").append(completeBox)
+                document.getElementById("axiosTodo").append(incompleteSpan)
+                document.getElementById("axiosTodo").append(incompleteBox)
                 document.getElementById("axiosTodo").appendChild(deleteButton)
 
-                if (response.data[i].completed === true) {
-                    title.classList.add("title")
-                }
+              // Put Complete
+              if (response.data[i].completed === true) {
+                title.classList.add("title")
+              }
+
+                var completedTask = {
+                  completed: true
+                };
+                var incompletedTask = {
+                  completed: false
+                };
+
+                var completedItem = document.getElementsByClassName("boxComplete")                
+                  completedItem[i].addEventListener("click", (event) => {
+                    event.preventDefault()
+                    id = response.data[i]._id
+                    axios.put(baseURL + id, completedTask)
+                      .then((response) => console.log(response.data))
+                      .catch((error) => console.log(error));
+                });
+              
+               // Put Incomplete
+              var boxIncomplete = document.getElementsByClassName("boxIncomplete")              
+              boxIncomplete[i].addEventListener("click", (event) => {
+                  event.preventDefault()
+                  id = response.data[i]._id
+                  axios.put(baseURL + id, incompletedTask)
+                    .then((response) => console.log(response.data))
+                    .catch((error) => console.log(error));
+                });
 
               // Delete            
               deleteButton.addEventListener("click", (event) => {
@@ -37,7 +78,7 @@ function get(){
                     .then((response) => console.log(response.data))
                     .catch((error) => console.log(error));
       });
-        }})
+  }})
 }
 get()
    
